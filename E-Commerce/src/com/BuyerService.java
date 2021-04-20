ec3ca5e4e3dd7205e935175fbd201f306989a35a
kasuni <kasunisamadara10@gmail.com>
2021-04-20 21:11:20 +0530

    Implement PUT method and call updateBuyer method in BuyerService class

--- E-Commerce/src/com/BuyerService.java
+++ E-Commerce/src/com/BuyerService.java
@@ -8,9 +8,13 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 @Path("/Buyer") 
 
 public class BuyerService 
@@ -44,4 +48,23 @@ public class BuyerService
     { 
        return BuyerObj.readBuyer(); 
     }
+   @PUT
+   @Path("/") 
+   @Consumes(MediaType.APPLICATION_JSON) 
+   @Produces(MediaType.TEXT_PLAIN) 
+   public String updateBuyer(String BuyerData) 
+   
+   { 
+   //Convert the input string to a JSON object 
+    JsonObject BuyerObject = new JsonParser().parse(BuyerData).getAsJsonObject(); 
+   //Read the values from the JSON object
+    String BuyerID = BuyerObject.get("Buyer ID").getAsString(); 
+    String BuyerCode = BuyerObject.get("Buyer Code").getAsString(); 
+    String BuyerName = BuyerObject.get("Buyer Name").getAsString(); 
+    String BuyerEmail = BuyerObject.get("Buyer Email").getAsString(); 
+    String BuyerContactNumber =BuyerObject.get("Buyer Contact Number").getAsString(); 
+    String BuyerAddress =BuyerObject.get("Buyer Address").getAsString(); 
+    String output = BuyerObj.updateBuyer(BuyerID,BuyerCode, BuyerName, BuyerEmail,BuyerContactNumber,BuyerAddress);
+    return output;
+   }
 }
