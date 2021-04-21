fcfba1f5f0797628e79682c09d499a88f2162bd7
kasuni <kasunisamadara10@gmail.com>
2021-04-21 14:01:58 +0530

    Fixed errors in updateBuyer and deleteBuyer methods

--- E-Commerce/src/com/BuyerService.java
+++ E-Commerce/src/com/BuyerService.java
@@ -37,10 +37,14 @@ public class BuyerService
     @FormParam("Buyer Contact Number") int BuyerContactNumber,
     @FormParam("Buyer Address") String BuyerAddress)
    
+   
    { 
 	 System.out.println("Hello");  
+	 
+	 System.out.println("A");
      String output= BuyerObj.insertBuyer(BuyerCode, BuyerName, BuyerEmail, BuyerContactNumber,BuyerAddress);
      
+     System.out.println("B");
      System.out.println(BuyerCode);
      System.out.println(BuyerName);
      return output;
@@ -52,7 +56,8 @@ public class BuyerService
    @Produces(MediaType.TEXT_HTML) 
     public String readBuyer()
     { 
-       return BuyerObj.readBuyer(); 
+       return BuyerObj.readBuyer();
+       //System.out.println("C");
     }
    
    @PUT
@@ -62,8 +67,11 @@ public class BuyerService
    public String updateBuyer(String BuyerData) 
    
    { 
+	   System.out.println("h");
    //Convert the input string to a JSON object 
     JsonObject BuyerObject = new JsonParser().parse(BuyerData).getAsJsonObject(); 
+    
+    System.out.println("i");
    //Read the values from the JSON object
     String BuyerID = BuyerObject.get("Buyer ID").getAsString(); 
     String BuyerCode = BuyerObject.get("Buyer Code").getAsString(); 
@@ -72,11 +80,14 @@ public class BuyerService
     String BuyerContactNumber =BuyerObject.get("Buyer Contact Number").getAsString(); 
     String BuyerAddress =BuyerObject.get("Buyer Address").getAsString(); 
     int BuyerContactNum = Integer.parseInt(BuyerContactNumber); 
+    int BuyerId=Integer.parseInt(BuyerID);
+    System.out.println("j");
     
     
-    String output = BuyerObj.updateBuyer(BuyerID,BuyerCode, BuyerName, BuyerEmail,BuyerContactNum,BuyerAddress);
+    String output = BuyerObj.updateBuyer(BuyerId,BuyerCode, BuyerName, BuyerEmail,BuyerContactNum,BuyerAddress);
     return output;
    }
+   
    @DELETE
    @Path("/") 
    @Consumes(MediaType.APPLICATION_XML) 
@@ -84,11 +95,18 @@ public class BuyerService
    public String deleteBuyer(String BuyerData) 
    { 
    //Convert the input string to an XML document
-    org.jsoup.nodes.Document doc = Jsoup.parse( BuyerData, "", Parser.xmlParser()); 
+    org.jsoup.nodes.Document doc = Jsoup.parse( BuyerData, "", Parser.xmlParser());
+    System.out.println("cat");
+    System.out.println(doc.toString());
+
     
    //Read the value from the element <itemID>
-    String BuyerID = doc.select("Buyer ID").text();
+    String BuyerID = doc.select("BuyerID").text();
+    System.out.println("dog :"+BuyerID);
+    
+    
     String output = BuyerObj.deleteBuyer(BuyerID);
+   
     return output; 
    }
    
